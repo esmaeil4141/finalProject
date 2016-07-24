7d0f6fd4356d6624452d7b7881c3e584900cac02
M3hRaN <m3hrantorki@gmail.com>
2016-07-24 19:38:14 +0330

    fix Message class race condition

--- app/src/main/java/io/sharif/pavilion/network/DataStructures/Message.java
+++ app/src/main/java/io/sharif/pavilion/network/DataStructures/Message.java
@@ -7,14 +7,12 @@ import java.util.List;
 
 public class Message {
 
-    private final Object uRIListLock;
     private List<Uri> fileUris;
     private String message;
     private int ID;
 
     public Message(int ID) {
         this.ID = ID;
-        this.uRIListLock = new Object();
     }
 
     public int getID() {
@@ -34,15 +32,11 @@ public class Message {
     }
 
     public synchronized void addUri(Uri uri) {
-        synchronized (uRIListLock) {
-            if (fileUris == null) fileUris = new ArrayList<>();
-            fileUris.add(uri);
-        }
+        if (fileUris == null) fileUris = new ArrayList<>();
+        fileUris.add(uri);
     }
 
     public synchronized boolean removeUri(Uri uri) {
-        synchronized (uRIListLock) {
-            return fileUris != null && fileUris.remove(uri);
-        }
+        return fileUris != null && fileUris.remove(uri);
     }
 }
