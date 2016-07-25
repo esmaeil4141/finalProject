461ae98eb93e7ed223981004a15f352bcee01c03
esmaeil4141 <esmaeil414@gmail.com>
2016-07-25 12:10:46 +0430

    testing listeners

--- app/src/main/java/io/sharif/pavilion/clientSide/HeadlinesFragment.java
+++ app/src/main/java/io/sharif/pavilion/clientSide/HeadlinesFragment.java
@@ -5,14 +5,26 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import io.sharif.pavilion.R;
 import io.sharif.pavilion.model.ServerObj;
+import io.sharif.pavilion.model.contents.ContentsObj;
+import io.sharif.pavilion.network.DataStructures.ApInfo;
+import io.sharif.pavilion.network.DataStructures.Message;
+import io.sharif.pavilion.network.Listeners.ClientListener;
+import io.sharif.pavilion.network.Listeners.ReceiveMessageListener;
+import io.sharif.pavilion.network.Listeners.WifiListener;
+import io.sharif.pavilion.network.Listeners.WifiScanListener;
+import io.sharif.pavilion.network.Services.ClientService;
+import io.sharif.pavilion.network.Utilities.ActionResult;
 import io.sharif.pavilion.utility.Statics;
 
 
@@ -57,13 +69,92 @@ public class HeadlinesFragment extends Fragment {
 
         ArrayList<ServerObj> serverObjs= Statics.getFakeSavedServers(activity);
 
-        adapter = new MyAdapterC(serverObjs,activity,activity);
-        headRecyclerView.setAdapter(adapter);
-        //TODO layoutManager:
-        headRecyclerView.setLayoutManager(new LinearLayoutManager(activity));
-//        headRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, 1));
+        ClientService clientService=new ClientService(activity, new WifiScanListener() {
+            @Override
+            public void onWifiScanFinished(List<ApInfo> scanResults) {
+                Toast.makeText(activity,"scan finished",Toast.LENGTH_LONG).show();
+                List<ServerObj> s=new ArrayList<>();
+                for (ApInfo apInfo:scanResults){
+                 s.add(new ServerObj(apInfo,new ContentsObj()));
+                }
+                adapter = new MyAdapterC(s, activity, activity);
+                headRecyclerView.setAdapter(adapter);
+                //TODO layoutManager:
+                headRecyclerView.setLayoutManager(new LinearLayoutManager(activity));
+            }
 
-//        setListAdapter(new ArrayAdapter<String>(getActivity(),layout,data));
+            @Override
+            public void onFailure(ActionResult errorCode) {
+
+            }
+        },
+                new ClientListener() {
+                    @Override
+                    public void onJoinedGroup() {
+pr("onJoinedGroup");
+                    }
+
+                    @Override
+                    public void onConnected() {
+pr("onConnected");
+                    }
+
+                    @Override
+                    public void onDisconnected() {
+pr("onDisconnected");
+                    }
+
+                    @Override
+                    public void onLeftGroup() {
+pr("onLeftGroup");
+                    }
+
+                    @Override
+                    public void onMessageReceived(Message message) {
+pr("onMessageReceived_message:"+message.getMessage());
+                    }
+                },
+                new WifiListener() {
+                    @Override
+                    public void onWifiEnabled() {
+pr("onWifiEnabled");
+                    }
+
+                    @Override
+                    public void onWifiDisabled() {
+pr("onWifiDisabled");
+                    }
+
+                    @Override
+                    public void onFailure(ActionResult errorCode) {
+pr("onFailure_errorCode:"+errorCode.name());
+                    }
+                },
+                new ReceiveMessageListener() {
+                    @Override
+                    public void onReceiveStart() {
+pr("onReceiveStart");
+                    }
+
+                    @Override
+                    public void onProgress(float progress, float speed, long totalSize, long received) {
+pr("onProgress_ progress:"+progress+" speed:"+speed+ "  totalSize:"+totalSize+"  received:"+received);
+                    }
+
+                    @Override
+                    public void onReceiveFailure(ActionResult errorCode) {
+
+                    }
+                });
+        clientService.start();
+        Toast.makeText(activity,"starting scan...",Toast.LENGTH_LONG).show();
+        clientService.scan();
+
+//        adapter = new MyAdapterC(serverObjs,activity,activity);
+//        headRecyclerView.setAdapter(adapter);
+//        //TODO layoutManager:
+//        headRecyclerView.setLayoutManager(new LinearLayoutManager(activity));
+////        headRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, 1));
 
     }
 
@@ -93,5 +184,7 @@ public class HeadlinesFragment extends Fragment {
 //
 //    }
 
-
+public void pr(String msg){
+    Log.d("myPavilion",msg);
+}
 }
