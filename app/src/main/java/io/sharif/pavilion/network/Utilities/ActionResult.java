6f37be6b8538119b148350a689c9dfff83a336a9
M3hRaN <m3hrantorki@gmail.com>
2016-07-27 22:15:23 +0330

    Adding Java DOC

--- app/src/main/java/io/sharif/pavilion/network/Utilities/ActionResult.java
+++ app/src/main/java/io/sharif/pavilion/network/Utilities/ActionResult.java
@@ -1,8 +1,12 @@
 package io.sharif.pavilion.network.Utilities;
 
+/**
+ * Simple enum used for method invocation return value debugging.
+ */
 public enum ActionResult {
 
     SUCCESS,					// common success
     FAILURE,					// common failure
     UNKNOWN                     // unknown state
+
 }
--- app/src/main/java/io/sharif/pavilion/network/Utilities/Utility.java
+++ app/src/main/java/io/sharif/pavilion/network/Utilities/Utility.java
@@ -5,32 +5,40 @@ import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.Environment;
 import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 
 import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 
-import io.sharif.pavilion.clientSide.MainActivity;
 import io.sharif.pavilion.network.DataStructures.Message;
 import io.sharif.pavilion.network.Services.ServerService;
 
 public class Utility {
 
-    private static final String appFolderName = "HotSpot";
+    private static final String APP_FOLDER_NAME = "HotSpot";
     private static final int TCP_SEGMENT_SIZE = 8192;
+    private static final String TAG = "pavilion";
 
+    /**
+     * This method returns TCP segment size in bytes.
+     * @return TCP segment size
+     */
     public static int getTcpSegmentSize() {
         return TCP_SEGMENT_SIZE;
     }
 
-    public static void debugPrint(String str) {
-        Log.d("myTag", str);
+    /**
+     * This method logs input message for debugging.
+     * @param message message to be logged
+     */
+    public static void debugPrint(String message) {
+        Log.d(TAG, message);
     }
 
     /**
-     * Enable wifi.
+     * This method is used to enable wifi.
+     * @param context application context
      * @return {@code SUCCESS} if operation succeeds, {@code FAILURE} otherwise
      */
     public static ActionResult enableWifi(Context context) {
@@ -45,7 +53,8 @@ public class Utility {
     }
 
     /**
-     * Disable wifi.
+     * This method is used to disable wifi.
+     * @param context application context
      * @return {@code SUCCESS} if operation succeeds, {@code FAILURE} otherwise
      */
     public static ActionResult disableWifi(Context context) {
@@ -60,8 +69,9 @@ public class Utility {
     }
 
     /**
-     * Check whether or not wifi is enabled.
-     * @return {@code true} if WiFiManager is not null and wifi is enabled, {@code false} otherwise
+     * This method is used to check whether wifi is enabled.
+     * @param context application context
+     * @return {@code false} if context is null or wifi is disabled, {@code true} otherwise
      */
     public static boolean isWifiEnabled(Context context) {
         if (context != null) {
@@ -71,6 +81,11 @@ public class Utility {
         return false;
     }
 
+    /**
+     * This method is used to generate message MD5 hash string.
+     * @param message message to be generate hash from
+     * @return message MD5 hash
+     */
     public static String md5(String message) {
 
         StringBuilder hexString = null;
@@ -96,12 +111,22 @@ public class Utility {
         return hexString != null ? hexString.toString() : null;
     }
 
+    /**
+     * This method is used to generate wifi network password from it's name.
+     * @param ssid wifi network name
+     * @return password string
+     */
     public static String generatePassword(String ssid) {
         String hash = Utility.md5(ssid);
         return hash.length() < ServerService.PASSWORD_LENGTH
                 ? hash : hash.substring(0, ServerService.PASSWORD_LENGTH);
     }
 
+    /**
+     * This method is used to extract server name from it's ssid.
+     * @param ssid server ssid
+     * @return server name
+     */
     public static String getServerName(String ssid) {
         if (ssid != null) {
             return ssid.substring(
@@ -110,6 +135,12 @@ public class Utility {
         return null;
     }
 
+    /**
+     * This method is used to calculate message total length by summing text message length and all files size.
+     * @param context application context
+     * @param message message object
+     * @return total message length in bytes
+     */
     public static long getMessageTotalLength(Context context, Message message) {
 
         long totalLength = 0;
@@ -145,19 +176,29 @@ public class Utility {
         return totalLength;
     }
 
+    /**
+     * This method is used to get base address of app files.
+     * @return app base folder path
+     */
     public static String getAppFolderPath() {
-        return Environment.getExternalStorageDirectory() + File.separator + appFolderName + File.separator;
+        return Environment.getExternalStorageDirectory() + File.separator + APP_FOLDER_NAME + File.separator;
     }
 
     /**
-     * This method is used to run server listeners on UI thread.
-     * @param runnable runnable to be run on UI thread
+     * This method is used to post runnable to given thread. Typically it's used for posting on main thread.
+     * @param context context to post runnable to
+     * @param runnable runnable to be posted
      */
     public static void postOnMainThread(Context context, Runnable runnable) {
         Handler handler = new Handler(context.getMainLooper());
         handler.post(runnable);
     }
 
+    /**
+     * This method is used to convert IP address string to long value.
+     * @param ipAddress IP address string such as "192.168.1.1"
+     * @return long value of IP address
+     */
     public static long ipToLong(String ipAddress) {
 
         String[] ipAddressInArray = ipAddress.split("\\.");
@@ -174,15 +215,25 @@ public class Utility {
         return result;
     }
 
+    /**
+     * This method is used to calculate progress from total and sent/received bytes.
+     * @param total total bytes
+     * @param received total sent/received bytes
+     * @return progress percentage
+     */
     public static float calculateProgress(long total, long received) {
         if (total == 0) return 0;
         return ((float) received/total)*100;
     }
 
+    /**
+     * This method is used to calculate upload/download speed from number of bytes sent/received in an interval.
+     * @param milliTime the interval bytes sent/received
+     * @param bytes number of sent/received bytes
+     * @return upload/download speed in bytes per second
+     */
     public static float calculateSpeed(long milliTime, long bytes) {
         if (milliTime == 0) return 0;
-        Utility.debugPrint("Bytes : " + bytes + ", time : " + milliTime);
         return ((float)bytes/milliTime)*1000;
     }
-
 }
